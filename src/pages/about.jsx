26f87022e73578c62234ecfb718378e849de924c
Maxim Chen <chenr.maxim@gmail.com>
2023-05-12 16:03:41 -0700

    fix: about page/component import

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,34 +1,6 @@
 import React from "react";
-import Image from "next/image";
-import aboutPic from "@/public/assets/photos/about_me.jpg";
-import Link from "../components/Link";
+import About from "@/src/components/About";
 
-import styles from "@/src/styles/About.module.scss";
-
-const About = () => {
-  return (
-    <div className={styles.aboutContainer}>
-      <div className={styles.aboutPicContainer}>
-        <Image src={aboutPic} alt="about me picture" />
-      </div>
-      <div className={styles.aboutDescription}>
-        Now residing in the San Francisco Bay Area, I am a software engineer
-        primarily focused on front-end. I also come from a design background,
-        where I have a great appreciation for well-crafted aesthetics,
-        typography, graphic and print design.
-        <br />
-        <br />
-        Besides designing and coding, I&apos;m always discovering new bands and
-        artists, listening to music, practicing guitar, taking photos, making
-        coffee every morning and writing in my journal.
-        <br />
-        <div className={styles.aboutSocials}>
-          <Link href={"https://www.instagram.com/m520.n/"}>Instagram</Link>
-          <Link href={"https://open.spotify.com/user/121781429"}>Spotify</Link>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default About;
+export default function about() {
+  return <About />;
+}
