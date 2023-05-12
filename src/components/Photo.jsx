da45a5626c9e2a7cd794e17dbb2dd37c6c3844a5
Maxim Chen <chenr.maxim@gmail.com>
2023-05-12 16:25:11 -0700

    style: fix font-size on media queries
    styling: adjust navbar, photo container widths
    
    style: add padding-bottom to photography
    
    style: adjust photo sizing

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -12,7 +12,7 @@ const Photo = ({ photo }) => {
         fill
         src={photo.media_url}
         alt={photo.caption || "instagram photo caption"}
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        sizes="(max-width: 768px) 100vw, (max-width: 1440px) 50vw, 25vw"
       />
       <div className={styles.caption}>{photo.caption}</div>
     </div>
