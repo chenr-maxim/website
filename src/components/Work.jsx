62aed9c4e0d5940a722047c4ecb31640339353d9
Maxim Chen <chenr.maxim@gmail.com>
2023-06-29 15:00:46 -0700

    feat: add work experience labels and create work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { workExperience } from "../constants/labels";
+
+import styles from "@/src/styles/Work.module.scss";
 
 const Work = () => {
   return <div>Work</div>;
--- src/constants/labels.js
+++ src/constants/labels.js
@@ -20,3 +20,26 @@ export const navbarLinks = [
     title: "Photos",
   },
 ];
+
+export const workExperience = [
+  {
+    title: "Software Engineer - Front End",
+    company: "Hostfi",
+    date: "December 2022 - March 2023",
+    // description: "",
+  },
+  {
+    title: "Product Engineer - Front End",
+    company: "Kiddom",
+    date: "April 2021 - April July 2022",
+    description:
+      "Work on the Content team to integrate and create Teacher/Student interactive question-types(classifying, fill in the blank, categorizing, drawing etc.) and general instructional tools(auto-grading, performance analytics, curriculum/assignment planner) into the Kiddom platform using React and Typescript. Integrate digital curriculum ingested from Kiddom’s curriculum partners into the Kiddom platform. Build core classroom features such as Teacher/Student grading, analytic and curriculum planner tools into Kiddom’s classroom environment",
+  },
+  {
+    title: "Software Engineer",
+    company: "Change Healthcare",
+    date: "November 2018 - April 2020",
+    description:
+      "Worked on integrating Apple Pay JavaScript API into the Next Generation payment platform. As well as building a new UI for their payment platform checkout experience in React and React-Native. And created and integrated Change Healthcare APIs and their functionality into their Change Healthcare's Next Generation platform.",
+  },
+];
